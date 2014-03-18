e764a659a246cd61f4b9a1833b94f350335bfdd9
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2014-03-18 14:42:19 +0100

    Bumping the version since 0.1.0 is released now.

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
-(defproject io.screen6.cascading/redis "0.1.0"
+(defproject io.screen6.cascading/redis "0.1.1-SNAPSHOT"
   :description "Cascading tap for Redis"
   :url "http://opensource.screen6.io/"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
   :java-source-paths ["src/java"]
   :source-paths ["src/clj"]
-  :main io.screen6.cascading.redis.core
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
                                   [cascalog "2.0.1-SNAPSHOT"
                                    :exclusions [log4j/slf4j-api
@@ -16,5 +15,4 @@
   :repositories [["conjars" "http://conjars.org/repo/"]]
   :dependencies [[redis.clients/jedis "2.4.2"]
                  [cascading/cascading-core "2.5.2"]
-                 [org.slf4j/slf4j-api "1.7.6"]
-                 [ch.qos.logback/logback-classic "1.1.1"]])
+                 [org.slf4j/slf4j-api "1.7.6"]])
