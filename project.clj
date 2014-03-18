8eee6631d6787c83769f0fe0d005c0a3ee951015
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2014-03-18 11:15:37 +0100

    Clearing up dependencies.

--- project.clj
+++ project.clj
@@ -6,14 +6,13 @@
   :java-source-paths ["src/java"]
   :source-paths ["src/clj"]
   :main io.screen6.cascading.redis.core
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
+                                  [cascalog "2.0.1-SNAPSHOT"
+                                   :exclusions [log4j/slf4j-api
+                                                org.slf4j/slf4j-log4j12]]
+                                  [org.apache.hadoop/hadoop-core "1.2.1"]]}}
   :repositories [["conjars" "http://conjars.org/repo/"]]
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [redis.clients/jedis "2.4.2"]
+  :dependencies [[redis.clients/jedis "2.4.2"]
                  [cascading/cascading-core "2.5.2"]
                  [org.slf4j/slf4j-api "1.7.6"]
-                 [org.clojure/tools.logging "0.2.6"]
-                 [ch.qos.logback/logback-classic "1.1.1"]
-                 [cascalog "2.0.1-SNAPSHOT"
-                  :exclusions [log4j/slf4j-api
-                               org.slf4j/slf4j-log4j12]]
-                 [org.apache.hadoop/hadoop-core "1.2.1"]])
+                 [ch.qos.logback/logback-classic "1.1.1"]])
