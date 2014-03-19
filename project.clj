f574486cb4ab2ceee26a240f05263e21308cecee
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2014-03-19 14:34:08 +0100

    Small updates in project.clj.

--- project.clj
+++ project.clj
@@ -10,8 +10,11 @@
                                    :exclusions [log4j/slf4j-api
                                                 org.slf4j/slf4j-log4j12]]
                                   [org.apache.hadoop/hadoop-core "1.2.1"]
-                                  [com.taoensso/carmine "2.4.5"] ]
+                                  [com.taoensso/carmine "2.4.5"]
+                                  [ch.qos.logback/logback-classic "1.1.1"]
+                                  [leinjacker "0.4.1"]]
                    :plugins [[lein-midje "3.1.3"]]}}
+  :min-lein-version "2.3.0"
   :repositories [["conjars" "http://conjars.org/repo/"]]
   :dependencies [[redis.clients/jedis "2.4.2"]
                  [cascading/cascading-core "2.5.2"]
